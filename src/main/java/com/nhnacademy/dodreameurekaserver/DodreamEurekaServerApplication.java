764de5ecf6b89f6b9e9256c555f220d1884350d3
HJin <guswlsdl7771@naver.com>
2025-06-09 11:37:34 +0900

    Add eureka-server

---
+++ src/main/java/com/nhnacademy/dodreameurekaserver/DodreamEurekaServerApplication.java
@@ -0,0 +1,15 @@
+package com.nhnacademy.dodreameurekaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class DodreamEurekaServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DodreamEurekaServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/nhnacademy/dodreameurekaserver/DodreamEurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nhnacademy.dodreameurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DodreamEurekaServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
