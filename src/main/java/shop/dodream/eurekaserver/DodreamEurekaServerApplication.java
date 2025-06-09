570637981e2954315fe388c02c859ed58b79d8bd
HJin <guswlsdl7771@naver.com>
2025-06-09 16:33:05 +0900

    Eureka Main

--- src/main/java/shop/dodream/eurekaserver/DodreamEurekaServerApplication.java
+++ src/main/java/shop/dodream/eurekaserver/DodreamEurekaServerApplication.java
@@ -1,4 +1,4 @@
-package com.nhnacademy.dodreameurekaserver;
+package shop.dodream.eurekaserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
